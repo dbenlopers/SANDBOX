1cd74b589acc59a85c7469ad8bcccfc5683abbbb
KOPP Arnaud <kopp.arnaud@gmail.com>
2015-01-26 20:59:35 +0100

    play witj keep open function from book

--- C++/test.cpp
+++ C++/test.cpp
@@ -1,5 +1,11 @@
-#include <iostream>
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cmath>
 using namespace std;
+
+inline void keep_window_open() { char ch; cin>>ch; }
 
 int main()
 {
@@ -27,5 +33,6 @@ int main()
         {
                 cout << "your value is : " << x << endl ;
         }
+	keep_window_open();
         return 0;
 }
