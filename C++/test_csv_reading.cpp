18fb815fe015c2f758718ce21b92e2f192cab865
Arnaud KOPP <kopp.arnaud@gmail.com>
2015-09-24 15:25:24 +0200

    reading file for testing with csv_parser.h

--- C++/test_csv_reading.cpp
+++ C++/test_csv_reading.cpp
@@ -3,11 +3,17 @@
 #include <string>
 
 int main(){
-    io::CSVReader<2> in("/home/arnaud/Desktop/HDV/DATA/target_8.3.csv");
-    in.read_header(io::ignore_extra_column, "Well", "AvgIntenCh2");
-    std::string Well = "";
-    int AvgIntenCh2 = 0;
-    while(in.read_row(Well, AvgIntenCh2)){
-        std::cout << Well << " " << AvgIntenCh2  << std::endl;
+    // io::CSVReader<2> in("/home/arnaud/Desktop/HDV/DATA/target_8.3.csv");
+    // in.read_header(io::ignore_extra_column, "Well", "AvgIntenCh2");
+    // std::string Well = "";
+    // int AvgIntenCh2 = 0;
+    // while(in.read_row(Well, AvgIntenCh2)){
+    //     std::cout << Well << " " << AvgIntenCh2  << std::endl;
+    // }
+
+    io::LineReader in("/home/arnaud/Desktop/TEMP/final_Cufflinks_genes_HUVEC-2x75-10885-rep2.genes.gtf");
+    while(char*line = in.next_line()){
+        std::cout << line << std::endl;
     }
+    return 0;
 }
