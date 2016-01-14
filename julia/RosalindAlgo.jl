19f674269d0ecb996bef4e1edc47a4289540ca77
KOPP Arnaud <kopp.arnaud@gmail.com>
2016-01-14 13:11:13 +0100

    Move euler project problem to there lang directory, split sort algo into one file in julia

--- julia/RosalindAlgo.jl
+++ julia/RosalindAlgo.jl
@@ -12,47 +12,10 @@ function Fibonacci(n::Int)
         return getindex(Arr, n+1)
     end
 end
-println("Fibonacci")
-@time x = Fibonacci(200)
-@time x = Fibonacci(200)
-
-
-## InsertionSort
-function SwapArray!(arr::AbstractArray, i1::Int, i2::Int)
-    temp = getindex(arr, i1)
-    setindex!(arr, getindex(arr, i2), i1)
-    setindex!(arr, temp, i2)
-end
-# return movement count
-function InsertionSort(arr::AbstractArray)
-    mvcnt = 0
-    for i in 2:length(arr)
-        k = i
-        while k > 1 && (getindex(arr, k) < getindex(arr, k-1))
-            SwapArray!(arr, k-1, k)
-            k -= 1
-            mvcnt += 1
-        end
-    end
-    return mvcnt
-end
-println("Swap inside array")
-@time SwapArray!([6, 10, 4, 5, 1, 2], 4, 5)
-@time SwapArray!([6, 10, 4, 5, 1, 2], 4, 5)
-
-println("Insertion Sort")
-@time InsertionSort([6, 10, 4, 5, 1, 2])
-
-f = open("/home/akopp/Documents/RosalindInput/rosalind_ins.txt")
-x = readall(f)
-x = split(x, "\n")
-array = readdlm(IOBuffer(x[2]), Int)
-close(f)
-@time InsertionSort(array)
 
 
 
-## recursive binary search
+## recursive binary search, return index of value to search
 function RecBinarySearch(arr::AbstractArray, value, low, high)
     if high < low
         return -1
@@ -66,13 +29,9 @@ function RecBinarySearch(arr::AbstractArray, value, low, high)
         return mid
     end
 end
-println("Recursive binary search")
-@time x = RecBinarySearch([6, 10, 12, 20, 65], 10, 1, 6)
-println(x)
-@time x = RecBinarySearch([6, 10, 12, 20, 65, 78, 79, 85, 90], 85, 1, 9)
-println(x)
 
-## iterative binary search
+
+## iterative binary search, return index of value
 function ItBinarySearch(arr::AbstractArray, value)
     low = 1
     high = length(arr)
@@ -88,6 +47,22 @@ function ItBinarySearch(arr::AbstractArray, value)
     end
     return -1
 end
+
+
+####################### TEST
+
+println("Fibonacci")
+@time x = Fibonacci(200)
+@time x = Fibonacci(200)
+
+
+println("Recursive binary search")
+@time x = RecBinarySearch([6, 10, 12, 20, 65], 10, 1, 6)
+println(x)
+@time x = RecBinarySearch([6, 10, 12, 20, 65, 78, 79, 85, 90], 85, 1, 9)
+println(x)
+
+
 println("Iterative binary search")
 @time x = ItBinarySearch([6, 10, 12, 20, 65], 10)
 println(x)
@@ -105,6 +80,7 @@ searching = readdlm(IOBuffer(x[4]), Int)
 #     print(x)
 #     print(" ")
 # end
+
 
 println("Use of Binary search")
 @time ItBinarySearch(array, 3008)
---
+++ julia/sort.jl
@@ -0,0 +1,106 @@
+## InsertionSort swap value into array
+function SwapArray!(arr::AbstractArray, i1::Int, i2::Int)
+    temp = getindex(arr, i1)
+    setindex!(arr, getindex(arr, i2), i1)
+    setindex!(arr, temp, i2)
+end
+
+# return sorted array
+function InsertionSort(arr::AbstractArray)
+    for i in 2:length(arr)
+        k = i
+        while k > 1 && (getindex(arr, k) < getindex(arr, k-1))
+            SwapArray!(arr, k-1, k)
+            k -= 1
+        end
+    end
+    return arr
+end
+
+# return sorted array and movement count
+function InsertionSortMovCnt(arr::AbstractArray)
+    mvcnt = 0
+    for i in 2:length(arr)
+        k = i
+        while k > 1 && (getindex(arr, k) < getindex(arr, k-1))
+            SwapArray!(arr, k-1, k)
+            k -= 1
+            mvcnt += 1
+        end
+    end
+    return arr, mvcnt
+end
+
+
+## merge sort
+function mergesort!(A::AbstractVector)
+    n = length(A)
+    if n <= 1
+        return A
+    end
+    Arr = Array(typeof(A[1]), length(A))
+    width = 1
+    while width < n
+        i = 1
+        while i < n
+            i += i+2*width
+            merge(A, i, min(i+width, n),min(i+2*width, n) ,Arr)
+        end
+        copyarray(Arr, A, n)
+        width *= 2
+    end
+end
+
+function merge(a::AbstractVector, left, right, final, b::AbstractVector)
+    i = left
+    j = right
+    for k = left:final
+        if (i < right) && (j >= final || a[i] <= a[j])
+            b[k] = a[i]
+            i += 1
+        else
+            b[k] = a[j]
+            j += 1
+        end
+    end
+end
+
+####################### TEST
+
+# copy array b into array a
+function copyarray(b::AbstractVector, a::AbstractVector, n)
+    @inbounds for i = 1:n
+        a[i] = b[i]
+    end
+end
+
+println("Swap inside array")
+@time SwapArray!([6, 10, 4, 5, 1, 2], 4, 5)
+@time SwapArray!([6, 10, 4, 5, 1, 2], 4, 5)
+
+println("Insertion Sort")
+A = randcycle(10000)
+@time InsertionSort(A)
+A = randcycle(10000)
+@time InsertionSort(A)
+
+f = open("/home/akopp/Documents/RosalindInput/rosalind_ins.txt")
+x = readall(f)
+x = split(x, "\n")
+array = readdlm(IOBuffer(x[2]), Int)
+close(f)
+@time InsertionSort(array)
+
+A = randcycle(10000)
+println("Merge Sort")
+A = randcycle(10000)
+@time mergesort!(A)
+A = randcycle(10000)
+@time mergesort!(A)
+
+
+println("Julia sort")
+A = randcycle(10000)
+@time sort!(A)
+A = randcycle(10000)
+@time sort!(A)
